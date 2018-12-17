5566dbfe2119da2521fe01ad0c3934e12a78df7e
Kim-DongYoung <op2gs2@gmail.com>
2018-12-17 10:42:42 +0900

    Update 실습4.c

--- week12/실습4.c
+++ week12/실습4.c
@@ -17,11 +17,11 @@ int main(void) {
 	,{3,4,5,6,7} }; //2차원 배열 선언
 	int rowsum[5] = { 0 }; //열을 더함
 	int colsum[5] = { 0 }; //행을 더함
-
+	int i, j; //반복문의 제어 변수 (variable for iteration)
 	sumarray(array,rowsum,colsum); //함수의 호출
 
 	//배열을 출력. 행과 열의 합도 출력
-	int i, j;
+	
 	for (i = 0; i < 5; i++) {
 		for (j = 0; j < 5; j++) {
 			printf("%d\t", array[i][j]); //배열 출력
